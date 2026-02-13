dee509627032b00f8f73cb115de521b9b8bb3cee
nahzulbalaghoh18 <nahzulbalaghoh18@gmail.com>
2026-02-13 14:59:50 +0700

    P1_Praktikum2

---
+++ P1/bintang.java
@@ -0,0 +1,34 @@
+package P1;
+import java.util.Scanner;
+public class bintang {
+    public static void main(String[] args) {
+        Scanner nahzul = new Scanner(System.in);
+
+        System.out.print("Masukkan NIM: ");
+        double nim = nahzul.nextDouble();
+
+        int n = (int)(nim % 100);
+
+        if ( n < 10) {
+            n += 10;
+        }
+
+        System.out.println("maka n = " + n);
+        System.out.print("Output: ");
+
+        for (int i = 1; i <= n; i++) {
+            if (i==10 || i==15) {
+                continue;
+            }
+            if (i % 3 == 0) {
+                System.out.print("#");
+            } else if (i % 2 == 0) {
+                System.out.print(i);
+            } else {
+                System.out.print("*");
+            }
+        }
+
+        nahzul.close();
+    }
+}
