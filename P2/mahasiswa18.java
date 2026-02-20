d0040efc547733978e3228d43f98d93c1ffade5f
nahzulbalaghoh18 <nahzulbalaghoh18@gmail.com>
2026-02-20 15:25:25 +0700

    P2_Praktikum1_Pertanyaan4

--- P2/mahasiswa18.java
+++ P2/mahasiswa18.java
@@ -15,7 +15,11 @@ public class mahasiswa18 {
     }
 
     void updateIPk (double ipkBaru){
-        ipk = ipkBaru;
+        if (ipk >= 0.0 && ipk <= 4.0) {
+            ipk = ipkBaru;
+        }else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
     }
 
     String nilaiKinerja(){
@@ -30,14 +34,4 @@ public class mahasiswa18 {
         }
     }
 
-    public mahasiswa18(){
-
-    }
-
-    public mahasiswa18(String nm, String nim, double ipk, String kls){
-        nama = nm;
-        this.nim = nim;
-        this.ipk = ipk;
-        kelas = kls;
-    }
 }
