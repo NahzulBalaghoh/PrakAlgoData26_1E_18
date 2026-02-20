678a09988a679ec4b702020825705212f68808f3
nahzulbalaghoh18 <nahzulbalaghoh18@gmail.com>
2026-02-20 13:41:19 +0700

    P2_Praktikum2

---
+++ P2/mahasiswaMain18.java
@@ -0,0 +1,16 @@
+package P2;
+public class mahasiswaMain18 {
+    public static void main(String[] args) {
+        mahasiswa18 mhsl = new mahasiswa18();
+        mhsl.nama = "Muhammad Ali Farhan";
+        mhsl.nim = "2241720171";
+        mhsl.kelas = "SI 2J";
+        mhsl.ipk = 3.55;
+
+        mhsl.tampilkanInformasi();
+        mhsl.ubahKelas("SI 2K");
+        mhsl.updateIPk(3.60);
+        mhsl.tampilkanInformasi();
+
+    }
+}
