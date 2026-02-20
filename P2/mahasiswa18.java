e7f785015f4b4fd49e168c05b453b50623ab2cea
nahzulbalaghoh18 <nahzulbalaghoh18@gmail.com>
2026-02-20 13:44:58 +0700

    P2_Praktikum3

--- P2/mahasiswa18.java
+++ P2/mahasiswa18.java
@@ -29,4 +29,15 @@ public class mahasiswa18 {
             return "Kinerja Kurang";
         }
     }
+
+    public mahasiswa18(){
+
+    }
+
+    public mahasiswa18(String nm, String nim, double ipk, String kls){
+        nama = nm;
+        this.nim = nim;
+        this.ipk = ipk;
+        kelas = kls;
+    }
 }
