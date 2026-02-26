17c618ea0ce11471cf80127d7fe51c194f20731f
nahzulbalaghoh18 <nahzulbalaghoh18@gmail.com>
2026-02-26 11:04:29 +0700

    P3_Praktikum3_Pertanyaan2

--- P3/mataKuliah18.java
+++ P3/mataKuliah18.java
@@ -1,4 +1,5 @@
 package P3;
+import java.util.Scanner;
 
 public class mataKuliah18 {
 
@@ -7,10 +8,27 @@ public class mataKuliah18 {
     public int sks;
     public int jumlahJam;
 
+
     public mataKuliah18 (String kode, String nama, int sks, int jumlahJam){
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+
+    public void tambahData (Scanner nahzul){
+        System.out.print("Kode        : ");
+        this.kode = nahzul.nextLine();
+        System.out.print("Nama        : ");
+        this.nama = nahzul.nextLine();
+        System.out.print("Sks         : ");
+        this.sks = Integer.parseInt(nahzul.nextLine());
+        System.out.print("Jumlah Jam  : ");
+        this.jumlahJam = Integer.parseInt(nahzul.nextLine());
+        System.out.println("=======================================================");
+    }
+
+    public mataKuliah18(){
+
     }
 }
--- P3/mataKuliahDemo18.java
+++ P3/mataKuliahDemo18.java
@@ -6,25 +6,11 @@ public class mataKuliahDemo18 {
     public static void main(String[] args) {
         Scanner nahzul = new Scanner(System.in);
         mataKuliah18 [] arrayOfMatakuliah = new mataKuliah18[3];
-        String kode, nama, dummy;
-        int sks, jumlahJam;
 
         for (int i = 0; i < 3; i++) {
             System.out.println("Masukkan Data Matakuliah ke-"+(i+1));
-            System.out.print("Kode          : ");
-            kode = nahzul.nextLine();
-            System.out.print("Nama          : ");
-            nama = nahzul.nextLine();
-            System.out.print("Sks           : ");
-            dummy = nahzul.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah Jam    : ");
-            dummy = nahzul.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
-            System.out.println("---------------------------------------------");
-
-
-            arrayOfMatakuliah[i] = new mataKuliah18(kode, nama, sks, jumlahJam);
+            arrayOfMatakuliah[i] = new mataKuliah18();
+            arrayOfMatakuliah[i].tambahData(nahzul);
         }
 
         for (int i = 0; i < 3; i++) {
