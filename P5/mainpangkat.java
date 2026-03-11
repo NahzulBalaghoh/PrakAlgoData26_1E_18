4e3787ccad431c6fe5019fa87e824f8e556cad51
nahzulbalaghoh18 <nahzulbalaghoh18@gmail.com>
2026-03-12 02:54:01 +0700

    P5_Praktikum2

---
+++ P5/mainpangkat.java
@@ -0,0 +1,30 @@
+package P5;
+import java.util.Scanner;
+public class mainpangkat {
+
+    public static void main(String[] args) {
+        Scanner nahzul = new Scanner (System.in);
+        System.out.print("Masukkan jumlah elemen: ");
+        int elemen =nahzul.nextInt();
+
+        pangkat[] png = new pangkat[elemen];
+        for (int i = 0; i < elemen; i++) {
+            System.out.print("Masukkan nilai basis elemen ke-"+(i+1)+ ": ");
+            int basis = nahzul.nextInt();
+            System.out.print("Masukkan nilai pangkat elemen ke-"+ (i+1)+ ": ");
+            int pangkat = nahzul.nextInt();
+            png [i] = new pangkat(basis, pangkat);
+        }
+
+        System.out.println("HASIL PANGKA BRUTEFORCE: ");
+        for (pangkat p: png) {
+            System.out.println(p.nilai + "^" + p.pangkat + ": " + p.pangkatBF(p.nilai, p.pangkat));
+        }
+        System.out.println("HASIL PANGKA DEVIDE AND CONQUER: ");
+        for (pangkat p: png) {
+            System.out.println(p.nilai + "^" + p.pangkat + ": " + p.pangkatDC(p.nilai, p.pangkat));
+        }
+
+        nahzul.close();
+    }
+}
