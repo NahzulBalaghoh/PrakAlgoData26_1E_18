46ba8b404720bbbd2f7ae6e40630274109d96f83
nahzulbalaghoh18 <nahzulbalaghoh18@gmail.com>
2026-03-12 01:51:57 +0700

    P5_Praktikum1

---
+++ P5/faktorial.java
@@ -0,0 +1,35 @@
+package P5;
+import java.util.Scanner;
+
+public class faktorial {
+
+    int faktorialBf (int n){
+        int fakto =1;
+        for (int i = 1; i <= n; i++) {
+            fakto = fakto * i;
+        }
+        return fakto;
+    }
+
+    int faktorialDC (int n){
+        if (n == 1) {
+            return 1;
+        }else {
+            int fakto = n * faktorialDC (n-1);
+            return fakto;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner nahzul = new Scanner (System.in);
+        System.out.print("Masukkan nilai: ");
+        int nilai = nahzul.nextInt();
+
+        faktorial fk = new faktorial ();
+        System.out.println("Nilai faktorial "+nilai+" menggunakan BF: "+ fk.faktorialBf(nilai));
+        System.out.println("Nilai faktorial "+nilai + " menggunakan DC: "+fk.faktorialDC(nilai));
+
+        nahzul.close();
+    }
+
+}
