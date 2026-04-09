d2878371bcf00c61a77ccbf8ea799305e828748b
nahzulbalaghoh18 <nahzulbalaghoh18@gmail.com>
2026-04-09 21:12:23 +0700

    P6_Praktikum2

---
+++ P6/MahasiswaBerprestasi18.java
@@ -0,0 +1,38 @@
+package P6;
+
+
+public class MahasiswaBerprestasi18 {
+
+    Mahasiswa18 [] listMhs = new Mahasiswa18[5];
+    int idx;
+    
+    void tambah (Mahasiswa18 m){
+        if (idx < listMhs.length) {
+            listMhs[idx]= m;
+            idx ++;
+        }else {
+            System.out.println("data sudah penuh");
+        }
+    }
+
+    void tampil (){
+        for (Mahasiswa18 m : listMhs ) {
+            m.tampilInformasi();
+            System.out.println("-------------------------------");
+        }
+    }
+
+    void bubbleSort(){
+        for (int i = 0; i < listMhs.length; i++) {
+            for (int j = 1; j < listMhs.length; j++) {
+                if (listMhs[j].ipk>listMhs[j-1].ipk) {
+                    Mahasiswa18 tmp = listMhs[j];
+                    listMhs[j] = listMhs [j-1];
+                    listMhs[j-1]= tmp;
+                }
+            }
+        }
+    }
+
+}
+
