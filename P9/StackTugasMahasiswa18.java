be585f89dcda6ed475c08daf394674ae3eced6ab
nahzulbalaghoh18 <nahzulbalaghoh18@gmail.com>
2026-05-02 03:58:45 +0700

    P9_Praktikum2

--- P9/StackTugasMahasiswa18.java
+++ P9/StackTugasMahasiswa18.java
@@ -76,4 +76,18 @@ public class StackTugasMahasiswa18 {
     public int count (){
         return top + 1;
     }
+
+    public String konversiDecimalKeBiner (int nilai){
+        StackKonversi18 stack = new StackKonversi18();
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String ();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
+    }
 }
