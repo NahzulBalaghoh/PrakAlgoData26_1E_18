a40c432be5b13a0ce3d3fa731959378c7e80515b
nahzulbalaghoh18 <nahzulbalaghoh18@gmail.com>
2026-05-18 00:10:40 +0700

    P11_Praktikum2

--- P11/SLLMain18.java
+++ P11/SLLMain18.java
@@ -6,7 +6,34 @@ public class SLLMain18 {
         Scanner nahzul = new Scanner(System.in);
 
         SingleLinkedList18 sll = new SingleLinkedList18();
-       
+        
+        Mahasiswa18 mhs1 = new Mahasiswa18("21212203", "Dirga",  "4D", 3.6);
+        Mahasiswa18 mhs2 = new Mahasiswa18("24212200", "Alvaro", "1A", 4.0);
+        Mahasiswa18 mhs3 = new Mahasiswa18("22212202", "Cintia", "3C", 3.5);
+        Mahasiswa18 mhs4 = new Mahasiswa18("23212201", "Bimon",  "2B", 3.8);
+
+        sll.print();
+        sll.addLast(mhs1);
+        sll.print();
+        sll.addLast(mhs2);
+        sll.print();
+        sll.insertAfter("Dirga", mhs3);
+        sll.insertAt(2, mhs4);
+        sll.print();
+
+        System.out.println("data index 1 : ");
+        sll.getData(1);
+
+        System.out.println("data mahasiswa an Bimon berada pada index : " + sll.indexOf("bimon"));
+        System.out.println();
+
+        sll.removeFirst();
+        sll.removeLast();
+        sll.print();
+        sll.removeAt(0);
+        sll.print();
+
+
         System.out.println("Masukkan jumlah mahasiswa: ");
         int jml = Integer.parseInt(nahzul.nextLine());
         for (int i = 0; i < jml; i++) {
