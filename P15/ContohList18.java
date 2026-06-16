31e36bdbdea611006a95bd9a06f5d892612abb39
nahzulbalaghoh18 <nahzulbalaghoh18@gmail.com>
2026-06-16 21:56:57 +0700

    P15_Praktikum1_Pertanyaan3

--- P15/ContohList18.java
+++ P15/ContohList18.java
@@ -10,7 +10,7 @@ public class ContohList18 {
         l.add(1);
         l.add(2);
         l.add(3);
-        
+
         System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n",
                 l.get(0), l.size(), l.get(l.size() - 1));
 
@@ -19,7 +19,7 @@ public class ContohList18 {
         System.out.printf("Elemen 0: %d total elemen: %d elemen terakhir: %s\n",
                 l.get(0), l.size(), l.get(l.size() - 1));
 
-        List<String> names = new LinkedList<>();
+        LinkedList<String> names = new LinkedList<>();
         names.add("Noureen");
         names.add("Akhleema");
         names.add("Shannum");
